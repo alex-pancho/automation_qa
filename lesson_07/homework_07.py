2d3576e0c1759ba4fa8e57d7d86e80d286f2926e
Luffar <kanra512@gmail.com>
2023-12-05 19:22:01 +0200

    HM7 done

--- lesson_07/homework_07.py
+++ lesson_07/homework_07.py
@@ -11,13 +11,13 @@ def multiplication_table(number):
     while multiplier <= number:
         result = number * multiplier
         # десь тут помила, а може не одна
-        if  result > "25":
+        if  result > 25:
             # Enter the action to take if the result is greater than 25
-            pass
+            break
         print(str(number) + "x" + str(multiplier) + "=" + str(result))
 
         # Increment the appropriate variable
-        multi += 1
+        multiplier += 1
 
 multiplication_table(3)
 # Should print:
@@ -31,27 +31,55 @@ multiplication_table(3)
 # task 2
 """  Написати функцію, яка обчислює суму двох чисел.
 """
+def addit(a:int, b:int) -> int:
+    #Add two variables
+    addit_result = a + b
+
+    return addit_result
 
 
 # task 3
 """  Написати функцію, яка розрахує середнє арифметичне списку чисел.
 """
+def list_avg(nums_of_list:int) -> int:
+    #Calculate sum of all numbers
+    total_sum = sum(nums_of_list)
+    #Calculate average of numbers
+    avg = total_sum/len(nums_of_list)
+
+    return avg
 
 # task 4
 """  Написати функцію, яка приймає рядок та повертає його у зворотному порядку.
 """
+def revers_string(strg:str) -> str:
+    #Make type reversed
+    str_r = reversed(strg)
+    #Return to string
+    reversed_str = ''.join(str_r)
+
+    return reversed_str
 
 # task 5
 """  Написати функцію, яка приймає список слів та повертає найдовше слово у списку.
 """
+def long_word(words:str) -> str:
+    #Create empty variable type str
+    longest_word = ""
+    #Loop for found the longest word
+    for word in words:
+        if len(word) > len(longest_word):
+            longest_word = word
+
+    return longest_word
 
 # task 6
 """  Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
 у перший рядок, якщо другий рядок є підрядком першого рядка, та -1, якщо другий рядок
 не є підрядком першого рядка."""
-def find_substring(str1, str2):
-
-    return -1
+def find_substring(str1:str, str2:str) -> int:
+    #Returns index of the first match
+    return str1.find(str2)
 
 str1 = "Hello, world!"
 str2 = "world"
@@ -62,9 +90,42 @@ str2 = "cat"
 print(find_substring(str1, str2)) # поверне -1
 
 # task 7
+def uniq_elements(list_elements:list) -> set:
+    #Return unique elements
+    return set(list_elements)
+big_list = [3, 1, 4, 5, 2, 5, 3]
+print(uniq_elements(big_list))
 # task 8
+def revers_keys_value(key_value:dict) -> dict:
+    #Create empty dict
+    revers_new_dict = {}
+    #Change key to value
+    for v, k in key_value.items():
+        revers_new_dict[k] = v
+
+    return revers_new_dict
+
 # task 9
-# task 10
+def add_sets(set1:set, set2:set) -> set:
+    #Add unique sets element in sets_unique
+    sets_unique = set1 ^ set2
+
+    #Retorns sum of all elements in sets_unique
+    return sum(sets_unique)
+#task 10
+
+def sum_of_input_digits() -> int:
+    dig = input("Add digit: ")
+    sumNum = 0
+    if not dig.isdigit():
+        print("Enter digit!!!")
+    elif int(dig) == 0:
+        print("Digit must be above 0!")
+    else:
+        for i in dig:
+            sumNum = sumNum + int(i)
+    return sumNum
+print(sum_of_input_digits())
 """  Оберіть будь-які 4 таски з попередніх домашніх робіт та
 перетворіть їх у 4 функції, що отримують значення та повертають результат.
 Обоязково документуйте функції та дайте зрозумілі імена змінним.
