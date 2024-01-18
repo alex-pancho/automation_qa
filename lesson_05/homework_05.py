2bf930edaf1cf793641a4492b4044daaac8711db
Mikhailo <misadmitruha9@gmail.com>
2024-01-18 14:28:51 +0200

    Changes hm 05

--- lesson_05/homework_05.py
+++ lesson_05/homework_05.py
@@ -1,30 +1,37 @@
 small_list = [3, 1, 4, 5, 2, 5, 3]
 big_list = [3, 5, -2, -1, -3, 0, 1, 4, 5, 2]
 # task 1. Знайдіть всі унікальні елементи в списку small_list
-
+elements = set(small_list)
+print(elements)
 # task 2. Знайдіть середнє арифметичне всіх елементів у списку small_list
-
+average_small_list = sum(small_list) / len(small_list)
+print(average_small_list)
 # task 3. Перевірте, чи є в списку big_list дублікати
-
+duplicates_big_list = len(big_list) != len(set(big_list))
+print(duplicates_big_list)
 
 base_dict = {'contry':'Ukraine', 'continent': 'Europe', 'size': 123}
 add_dict = {"a":1, "b":2, "c":2, "d":3, 'size': 12}
 # task 4. Знайдіть ключ з максимальним значенням у словнику add_dict
-
+max_number = max(add_dict, key=add_dict.get)
+print(max_number)
 # task 5. Створіть новий словник, в якому ключі та значення будуть
 # замінені місцями у заданому словнику
-
+flip = {v: k for k, v in add_dict.items()}
+print(flip)
 # task 6. Об'єднайте два словника base_dict та add_dict  в новий словник sum_dict
 # Якщо ключі збігаються, то перетворіть значення в строку та об'єднайте їх
 sum_dict = {}
 
 # task 7.
 line = "Створіть множину всіх символів, які входять у заданий рядок"
-
+unique_chars_set = set(line)
+print(unique_chars_set)
 # task 8. Обчисліть суму елементів двох множин, які не є спільними
 set_1 = {1, 2, 3, 4, 5}
 set_2 = {4, 6, 5, 10}
-
+sum_of_unique_elements = sum(set_1.symmetric_difference(set_2))
+print(sum_of_unique_elements)
 # task 9. Створіть два списки та обробіть їх так, щоб отримати сет, який
 # містить всі елементи з обох списків,  які зустрічаються тільки один раз.
 # Наприклад, якщо перший список містить [1, 2, 3, 4], а другий
