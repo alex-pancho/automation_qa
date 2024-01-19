80780fb9375c8fc7544cba1e8c798e888a3792a5
glitvin <litgleb0@gmail.com>
2024-01-19 21:30:19 +0200

    litvin hm12

--- lesson_14/homework_14.py
+++ lesson_14/homework_14.py
@@ -30,3 +30,67 @@ else:
 
 Написати на це все тести
 """
+import unittest
+
+class SiteUser:
+    def __init__(self, name, email, access_level = "user"):
+        """
+        Constructor method to initialize user attributes
+        """
+        valid_access_levels = ["admin", "moderator", "user"]
+
+        self.name = name
+        self.email = email
+        self.access_level = access_level
+        self.logcount = 0
+
+    def __str__(self):
+        """
+        String representation of the user for easy printing
+        """
+        return f"The user {self.name}, Email: {self.email}, Access lvl: {self.access_level}"
+
+    def __eq__(self, other):
+        """
+        Equality comparison based on access levels
+        """
+        return self.access_level == other.access_level
+    def increment_logcount(self):
+        """
+        Method to increment the log count for the user
+        """
+        self.logcount += 1
+
+class TestSiteUser(unittest.TestCase):
+
+    user1 = SiteUser("John Doe", "john.doe@example.com", "user")
+    user2 = SiteUser("Jane Smith", "jane.smith@example.com", "admin")
+    user3 = SiteUser("Bob Johnson", "bob.johnson@example.com")
+
+    def test_string_representation_method(self):
+        expected_output = "The user John Doe, Email: john.doe@example.com, Access lvl: user"
+        self.assertEqual(str(self.user1), expected_output)
+
+    def test_eq_method_user_equal_to_itself(self):
+        self.assertTrue(self.user1 == self.user1)
+
+    def test_eq_method_users_with_different_access_levels(self):
+        self.assertFalse(self.user1 == self.user2)
+
+    def test_eq_method_users_with_same_access_levels(self):
+        user1_copy = SiteUser("John Doe", "john.doe@example.com", "user")
+        self.assertTrue(self.user1 == user1_copy)
+
+    def test_eq_method_users_with_default_access_level(self):
+        self.assertTrue(self.user1 == self.user3)
+
+    def test_increment_logcount_method(self):
+        self.assertEqual(self.user1.logcount, 0)
+        self.user1.increment_logcount()
+        self.assertEqual(self.user1.logcount, 1)
+        self.user1.increment_logcount()
+        self.assertEqual(self.user1.logcount, 2)
+
+if __name__ == '__main__':
+    unittest.main()
+
