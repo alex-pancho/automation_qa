a35e2f0e8ae8f3a0722d29041f02345dcadab02e
Mikhailo <misadmitruha9@gmail.com>
2024-02-12 17:12:47 +0200

    Changes hm 7

--- lesson_07/homework_07.py
+++ lesson_07/homework_07.py
@@ -11,13 +11,13 @@ def multiplication_table(number):
     while multiplier <= number:
         result = number * multiplier
         # десь тут помила, а може не одна
-        if  result > "25":
+        if  result > 25:
             # Enter the action to take if the result is greater than 25
-            pass
+            break
         print(str(number) + "x" + str(multiplier) + "=" + str(result))
 
         # Increment the appropriate variable
-        multi += 1
+        multiplier += 1
 
 multiplication_table(3)
 # Should print:
@@ -31,20 +31,25 @@ multiplication_table(3)
 # task 2
 """  Написати функцію, яка обчислює суму двох чисел.
 """
-
+def add_numbers(num1, num2):
+    return num1 + num2
 
 # task 3
 """  Написати функцію, яка розрахує середнє арифметичне списку чисел.
 """
+def calculate(numbers):
+    return sum(numbers) / len(numbers)
 
 # task 4
 """  Написати функцію, яка приймає рядок та повертає його у зворотному порядку.
 """
-
+def reverse                                     (input_str):
+    return input_str[::-1]
 # task 5
 """  Написати функцію, яка приймає список слів та повертає найдовше слово у списку.
 """
-
+def longest_word(words):
+    return max(words, key=len)
 # task 6
 """  Написати функцію, яка приймає два рядки та повертає індекс першого входження другого рядка
 у перший рядок, якщо другий рядок є підрядком першого рядка, та -1, якщо другий рядок
@@ -69,3 +74,61 @@ print(find_substring(str1, str2)) # поверне -1
 перетворіть їх у 4 функції, що отримують значення та повертають результат.
 Обоязково документуйте функції та дайте зрозумілі імена змінним.
 """
+
+# task 7
+"""  Задача з використанням циклу for та continue. Задано список фруктів 'fruits'
+потрібно вивести на екран всі елементи списку, окрім "orange".
+"""
+def print_fruits_except_orange(fruits):
+    for fruit in fruits:
+        if fruit == "orange":
+            continue
+        print(fruit)
+
+fruits = ["apple", "banana", "orange", "grape"]
+print_fruits_except_orange(fruits)
+
+# task 8
+"""  Уявіть, що інопланетянина з кольором alien_color щойно збили в грі.
+Створіть змінну під назвою alien_color і призначте їй значення 'green', 'yellow', або 'red'.
+Напишіть оператор if, щоб перевірити, чи колір прибульця 'green'.
+Якщо так, надрукуйте повідомлення про те, що гравець щойно заробив 5 балів.
+"""
+def check_alien_color(alien_color):
+    if alien_color == 'green':
+        print("Congratulations! You just earned 5 points.")
+
+alien_color = 'green' 
+check_alien_color(alien_color)
+
+# task 9
+"""  Начинки для піци (pizza_topping): напишіть цикл, який пропонує користувачеві ввести ряд начинок
+для піци, доки він не введе значення 'quit'. Коли вони введуть кожну начинку,
+надрукуйте повідомлення про те, що ви додасте цю начинку до їхньої піци.
+"""
+def make_pizza():
+    pizza_topping = ''
+    while pizza_topping != 'Не потрібно':
+        pizza_topping = input("Введіть начинку для піци (або 'Не потрібно' для завершення): ")
+        if pizza_topping != 'Не потрібно':
+            print("Додано " + pizza_topping + " до вашой піцци.")
+            
+make_pizza()
+
+# task 10
+"""  Напишіть програму, яка знаходить суму всіх цифр цілого числа, яке вводить користувач.
+Для перебору вводу від користувача використовуйте цикл. Виведіть суму цифр числа на екран.
+Приклад виконання програми:
+Введіть число: 12345
+Сума цифр числа 12345: 15
+"""
+
+def sum_of_digits():
+    user_number = input("Введіть цифри: ")
+    total = 0
+    for digit in user_number:
+        if digit.isdigit():
+            total += int(digit)
+    print("Сумма цифр", user_number, ":", total)
+
+sum_of_digits()
